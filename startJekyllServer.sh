04debafdb7d0955e8d8eaaf023df7bb7d74ffb42
David Poirier-Quinot <davipoir@gmail.com>
2018-05-13 00:18:14 +0200

    mod start bash to serve http to host ip (rather than localhost) for local access

--- startJekyllServer.sh
+++ startJekyllServer.sh
@@ -1,12 +1,12 @@
-# for Bundler setup, see: 
+# for Bundler setup, see:
 # https://help.github.com/articles/setting-up-your-github-pages-site-locally-with-jekyll/
-# 
+#
 # basically (if ruby installed):
 # > gem install bundler
 # > bundle install
 
 # launch localhost http server
-bundle exec jekyll serve --baseurl ''
+bundle exec jekyll serve --baseurl '' --port 8000 --host=0.0.0.0
 
 # open page in browser
 # open /Applications/Browsers/Google\ Chrome.app/ http://localhost:4000/
